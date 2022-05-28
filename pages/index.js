4abcb580ae019b9ecdb917b4239f42752410b9da
kalecream <sabrinamedwinter@gmail.com>
2022-05-28 18:20:54 -0500

    add minutes to read on blog

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,14 @@ export default function Home({ allPostsData }) {
       <section className={'blogSection'}>
         <h1 className={utilStyles.headingXL}>Blog</h1>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title, readTime }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                <Date dateString={date} /> ☆ <span id="time">{readTime}</span> minute read
               </small>
             </li>
           ))}
