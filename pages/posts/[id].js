5e79c4c5664ce00419e22c400c7a5c1ca1bfeea3
kalecream <sabrinamedwinter@gmail.com>
2022-05-27 23:07:24 -0500

    fix blog posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,13 +10,15 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
+      <div className={'post-container'}>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </div>
     </Layout>
   )
 }
