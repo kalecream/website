774fc4320c30e7e41a83d01cc377184fd68a8d03
kalecream <sabrinamedwinter@gmail.com>
2022-05-28 06:33:58 -0500

    change hero image url, add hero scrim

--- components/hero.js
+++ components/hero.js
@@ -13,58 +13,60 @@ import style from '../styles/modules/home.module.css'
 export default function HomeHero({ children, home }) {
   return (
     <section className={'hero'}>
-        <div className={'heroLeft'}>
-            <Image alt="" src="/images/illustration/green-ball.png" width={500} height={500}></Image>
-        </div>
+        <div className={'heroGradient'}>
+            <div className={'heroLeft'}>
+                <Image alt="" src="/images/illustration/green-ball.png" width={500} height={500}></Image>
+            </div>
+            <div className={'heroRight'}>
+                <div className={'glassBox'}>
+                    <div className={'heroText'}>
+                        <span className={'heroHighlight'}>
+                            Follow my
+                        </span>
+                        <h1 className={utilStyles.headingXL}>
+                            <span className={style.heroJourney}> </span>
+                        </h1> 
+                        <span className={'heroHighlight'}>
+                            Journey
+                        </span>
+                    </div>
 
-        <div className={'heroRight'}>
-            <div className={'glassBox'}>
-                <div className={'heroText frostedGlass rounded'}>
-                    <span className={'heroHighlight'}>
-                        Follow my
-                    </span>
-                    <h1 className={utilStyles.headingXL}>
-                        <span className={style.heroJourney}> </span>
-                    </h1> 
-                    <span className={'heroHighlight'}>
-                        Journey
-                    </span>
+                    <div className={'socialLinks'}>
+                    <Link href="https://twitter.com/KaleCream">
+                        <a>
+                            {/* <Twitter /> */}
+                            <Image src="/images/social/twitter.svg" width="30px" height="30px" alt="Twitter"></Image>
+                        </a>
+                    </Link>
+                    <Link href="https://mstdn.social/@KaleCream">
+                        <a>
+                        <Image src="/images/social/icons8-mastodon.svg" width="30px" height="30px" alt="Mastodon"></Image>
+                        </a>
+                    </Link>
+                    <Link href="https://www.deviantart.com/kalecream">
+                        <a>
+                        <Image src="/images/social/icons8-deviantart.svg" width="30px" height="30px" alt="Mastodon"></Image>
+                        </a>
+                    </Link>
+                    <Link href="https://github.com/kalecream">
+                        <a>
+                        <Image src="/images/social/icons8-github.svg" width="30px" height="30px" alt="Mastodon"></Image>
+                        </a>
+                    </Link>
+                    <Link href="http://www.linkedin.com/medwinter">
+                        <a>
+                        <Image src="/images/social/icons8-linkedin-circled.svg" width="30px" height="30px" alt="LinkedIn"></Image>
+                        </a>
+                    </Link>
+                    </div>  
+                    
+                    {/* <button className={'maxWidth '}>
+                        View the blog
+                    </button> */}
                 </div>
-
-                <div className={'socialLinks frostedGlass'}>
-                <Link href="https://twitter.com/KaleCream">
-                    <a>
-                        {/* <Twitter /> */}
-                        <Image src="/images/social/twitter.svg" width="30px" height="30px" alt="Twitter"></Image>
-                    </a>
-                </Link>
-                <Link href="https://mstdn.social/@KaleCream">
-                    <a>
-                    <Image src="/images/social/icons8-mastodon.svg" width="30px" height="30px" alt="Mastodon"></Image>
-                    </a>
-                </Link>
-                <Link href="https://www.deviantart.com/kalecream">
-                    <a>
-                    <Image src="/images/social/icons8-deviantart.svg" width="30px" height="30px" alt="Mastodon"></Image>
-                    </a>
-                </Link>
-                <Link href="https://github.com/kalecream">
-                    <a>
-                    <Image src="/images/social/icons8-github.svg" width="30px" height="30px" alt="Mastodon"></Image>
-                    </a>
-                </Link>
-                <Link href="http://www.linkedin.com/medwinter">
-                    <a>
-                    <Image src="/images/social/icons8-linkedin-circled.svg" width="30px" height="30px" alt="LinkedIn"></Image>
-                    </a>
-                </Link>
-                </div>  
-                
-                {/* <button className={'maxWidth '}>
-                    View the blog
-                </button> */}
             </div>
         </div>
+        
     </section>
   )
 }
